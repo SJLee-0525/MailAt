cb68b4ca1b97b8803102bd9aa92bae0e92082c2d
ChangHyunIm <dolong120408@naver.com>
2025-05-07 13:24:10 +0900

    Refactor : SMTP, IMAP napi 래핑

--- apps/electron-client/addon/unit_test/base64_test.cjs
+++ apps/electron-client/addon/unit_test/base64_test.cjs
@@ -3,3 +3,5 @@ const addon = require('../build/Release/mailio_addon.node');
 const base64 = new addon.Base64Wrapper();
 const result = base64.encode("Hello, World!");
 console.log(result);
+
+console.log(base64.decode(result));
