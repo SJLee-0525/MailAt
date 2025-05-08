af1e96b5f45aac2d2c2dba100251e543d4792f60
지우 <emily1290@naver.com>
2025-05-08 13:36:17 +0900

    feat: user CRUD 기능 개발

--- apps/electron-client/preload.js
+++ apps/electron-client/preload.js
@@ -1,14 +1,22 @@
 // src/preload.js
-const { contextBridge, ipcRenderer } = require('electron');
+const { contextBridge, ipcRenderer } = require("electron");
 
 // 메인 프로세스와의 안전한 통신을 위한 API 노출
-contextBridge.exposeInMainWorld('electronAPI', {
+contextBridge.exposeInMainWorld("electronAPI", {
   // 이메일 전송
-  sendEmail: (emailData) => ipcRenderer.invoke('email:send', emailData),
-  
+  sendEmail: (emailData) => ipcRenderer.invoke("email:send", emailData),
   // SMTP 연결 테스트
-  testSmtpConnection: (config) => ipcRenderer.invoke('smtp:connect', config),
-  
+  testSmtpConnection: (config) => ipcRenderer.invoke("smtp:connect", config),
   // SMTP 인증 테스트
-  testSmtpAuthentication: (config) => ipcRenderer.invoke('smtp:authenticate', config)
+  testSmtpAuthentication: (config) =>
+    ipcRenderer.invoke("smtp:authenticate", config),
+
+  // 사용자 관련 API
+  user: {
+    create: (userData) => ipcRenderer.invoke("user:create", userData),
+    get: (userId) => ipcRenderer.invoke("user:get", userId),
+    update: (userId, userData) =>
+      ipcRenderer.invoke("user:update", { userId, userData }),
+    delete: (userId) => ipcRenderer.invoke("user:delete", userId),
+  },
 });
